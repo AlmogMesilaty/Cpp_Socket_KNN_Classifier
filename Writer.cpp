40cc37bf442ca45396d43eda60f202d2328074c6
AlmogMesilaty <almog686@gmail.com>
2023-01-25 13:01:36 +0200

    Remove # from end of string

--- Writer.cpp
+++ Writer.cpp
@@ -8,13 +8,18 @@ Writer::Writer(DefaultIO* dio) {
 void Writer::writeToFIle() {
     std::string s = this->dio->read();
     std::ofstream out("temp.csv");
+    int endFlag = 0;
     if (!out) {
         dio->write("Cannot create file temp");
     }
     while(true) {
-        out << s;
         int index = s.find(END);
-        if(index != -1) {
+        if (index != -1) {
+            endFlag = 1;
+            s[index] = '\n';
+        }
+        out << s;
+        if(endFlag) {
             break;
         }
         s = this->dio->read();
